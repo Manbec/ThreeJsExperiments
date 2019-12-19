21a3e3e95cdf949c0616ca579acb648d4447e0e1
Manuel Becerra <manuel.becerra@globant.com>
2019-12-19 16:24:45 -0600

    basic GUI concept

--- src/app/starhead/starhead.module.ts
+++ src/app/starhead/starhead.module.ts
@@ -5,12 +5,14 @@ import {NgxsModule} from '@ngxs/store';
 import {NgxsReduxDevtoolsPluginModule} from '@ngxs/devtools-plugin';
 import {NgxsLoggerPluginModule} from '@ngxs/logger-plugin';
 import {stateList} from './game/game-state/state-list';
+import {CommonModule} from '@angular/common';
 
 @NgModule({
   declarations: [
     StarheadComponent,
   ],
   imports: [
+    CommonModule,
     StarheadRoutingModule,
     NgxsModule.forRoot([...stateList]),
     NgxsLoggerPluginModule.forRoot(),
